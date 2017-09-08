63baaec7687c5ae1cef2c690cb622a1169432972
GabeCampbell1010 <gabecampbell1010@gmail.com>
2017-09-08 13:57:25 -0600

    Add project files.

---
+++ SimpleShoppingList/Controllers/ItemController.cs
@@ -0,0 +1,70 @@
+﻿using SimpleShoppingList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SimpleShoppingList.Controllers
+{
+    public class ItemController : ApiController
+    {
+
+        // POST: api/Item
+        public IHttpActionResult Post([FromBody]Item item)//add item to shopping list
+        {
+            ShoppingList shoppingList = ShoppingListController.shoppingLists.Where(s => s.Id == item.ShoppingListId).FirstOrDefault();
+
+            if(shoppingList == null)//if can't find shopping list then 404
+            {
+                return NotFound();
+            }
+
+            item.Id = shoppingList.Items.Max(i => i.Id) + 1;//search for max id and increase by 1 to get unique id
+
+            shoppingList.Items.Add(item);//else add item to list
+
+            return Ok(shoppingList);//and return list with new item
+        }
+
+        // PUT: api/Item/5
+        public IHttpActionResult Put(int id, [FromBody]Item item)//update items
+        {
+            ShoppingList shoppingList = ShoppingListController.shoppingLists.Where(s => s.Id == item.ShoppingListId).FirstOrDefault();
+
+            if (shoppingList == null)//if can't find shopping list then 404
+            {
+                return NotFound();
+            }
+
+            Item changedItem = shoppingList.Items.Where(i => i.Id == id).FirstOrDefault();//look for item on server, if match
+
+            if(changedItem == null)
+            {
+                return NotFound();
+            }
+
+            changedItem.Checked = item.Checked;//set check property to item given
+
+            return Ok(shoppingList);
+        }
+
+        // DELETE: api/Item/5
+        public IHttpActionResult Delete(int id)//remove items
+        {
+            ShoppingList shoppingList = ShoppingListController.shoppingLists[0];
+
+            Item item = shoppingList.Items.FirstOrDefault(i => i.Id == id);
+
+            if(item == null)
+            {
+                return NotFound();
+            }
+
+            shoppingList.Items.Remove(item);
+
+            return Ok(shoppingList);
+        }
+    }
+}
---
+++ SimpleShoppingList/Scripts/app.js
@@ -0,0 +1,172 @@
+﻿
+var currentList = {};
+
+function createShoppingList() {
+    
+    currentList.name = $("#shoppingListName").val();
+    currentList.items = new Array();
+
+    //web service call
+    $.ajax({//this is getting the shopping list from the internet or something
+        type: "POST",
+        dataType: "json",
+        url: "api/ShoppingListsEF/",
+        data: currentList,
+        success: function (result) {
+            currentList = result;
+            showShoppingList();
+            history.pushState({ id: result.id }, result.name, "?id=" + result.id);
+        }
+    });  
+}
+
+function showShoppingList() {
+    $("#shoppingListTitle").html(currentList.name);
+    $("#shoppingListItems").empty();
+
+    $("#createListDiv").hide();
+    $("#shoppingListDiv").show();
+
+    $("#newItemName").val("");
+    $("#newItemName").focus();//focus cursor in new item box
+    $("#newItemName").unbind("keyup");
+    $("#newItemName").keyup(function (event) {
+        if (event.keyCode === 13) {//on enter make the new item so dont have to click button
+            addItem();
+        }
+    });
+
+
+}
+
+function addItem() {
+    var newItem = {};
+    newItem.name = $("#newItemName").val();//take the name from the input box with id newItemName, # is how you call by id
+    newItem.shoppingListId = currentList.id;
+
+    $.ajax({//this is getting the shopping list from the internet or something
+        type: "POST",
+        dataType: "json",
+        url: "api/ItemsEF/",//cuz item controller
+        data: newItem,
+        success: function (result) {
+            currentList = result;
+            drawItems();//call draw items function
+            $("#newItemName").val("");//trying to clear the input box after adding an item
+        }
+    });    
+}
+
+function drawItems() {
+    var $list = $("#shoppingListItems").empty();
+
+    for (var i = 0; i < currentList.items.length; i++) {
+        var currentItem = currentList.items[i];
+        var $li = $("<li>").html(currentItem.name).attr("id", "item_" + i);//create li tags with the item name between them and id="item_i" where i is the number
+        var $deleteBtn =
+            $("<button onclick='deleteItem(" + currentItem.id + ")'>D</button>").appendTo($li);//add two button tags between the li tags
+        var $checkBtn =
+            $("<button onclick='checkItem(" + currentItem.id + ")'>C</button>").appendTo($li);
+
+        if (currentItem.checked) {
+            $li.addClass("checked");
+        }
+
+        $li.appendTo($list);//add this newly created li tags and its internal parts to the list, which is the shoppinglistitems list
+
+    }
+}
+
+function deleteItem(itemId) {
+    $.ajax({//this is getting the shopping list from the internet or something
+        type: "DELETE",
+        dataType: "json",
+        url: "api/ItemsEF/" + itemId,//cuz item controller
+        success: function (result) {
+            currentList = result;
+            drawItems();//call draw items function
+        }
+    });
+}
+
+
+function checkItem(itemId) {//check and uncheck items on the list when clicking C button
+    var changedItem = {};
+
+    for (var i = 0; i < currentList.items.length; i++) {
+        if (currentList.items[i].id === itemId) {//why does it want === and not ==
+            changedItem = currentList.items[i];
+        }
+    }
+
+    changedItem.checked = !changedItem.checked;
+
+    $.ajax({//this is getting the shopping list from the internet or something
+        type: "PUT",
+        dataType: "json",
+        url: "api/ItemsEF/" + itemId,//cuz item controller
+        data: changedItem,
+        success: function (result) {
+            changedItem = result;
+            drawItems();//call draw items function
+            
+        }
+    });
+}
+
+function getShoppingListById(id) {
+
+    $.ajax({//this is getting the shopping list from the internet or something
+        type: "GET",
+        dataType: "json",
+        url: "api/ShoppingListsEF/" + id,
+        success: function (result) {
+            currentList = result;
+            showShoppingList();
+            drawItems();
+        }//,
+        //error: function () {
+        //    console.error("error");//this just shows in the console, check jquery documentation for how to throw error cases better
+        //}
+    });
+
+}
+
+function hideShoppingList()
+{
+    $("#createListDiv").show();
+    $("#shoppingListDiv").hide();
+
+    $("#shoppingListName").val("");
+    $("#shoppingListName").focus();
+    $("#shoppingListName").unbind("keyup");
+    $("#shoppingListName").keyup(function (event) {
+        if (event.keyCode === 13) {
+            createShoppingList();
+        }
+    });
+}
+
+$(document).ready(function () {
+    console.info("ready now");
+
+    hideShoppingList();
+
+    var pageUrl = window.location.href;
+    var idIndex = pageUrl.indexOf("?id=");
+    if (idIndex !== -1) {//this is, if there is a unique id//!= or !==? VS doesn't like != here
+        getShoppingListById(pageUrl.substring(idIndex + 4));
+    }
+
+    window.onpopstate = function (event) {
+        if (event.state === null) {
+            hideShoppingList();
+        }
+        else {
+            getShoppingListById(event.state.id);
+        }
+    }
+});
+
+
+//i could change this to like taxi pickup schedule that checks off people you pick up and deletes them when you drop them off
